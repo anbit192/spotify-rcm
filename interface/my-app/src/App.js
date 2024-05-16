572919b175aa7e56860b981e72f306968a8bc679
godwindk3 <phongprosupper@gmail.com>
2024-05-16 20:28:12 +0700

    breakthrough interface

--- interface/my-app/src/App.js
+++ interface/my-app/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SearchPage from './pages/SearchPage';
+import SongDetailsPage from './pages/SongDetailsPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/song/:id" element={<SongDetailsPage />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
