242de0f1996326a31065a13d5eb9f39da6ed5d47
anbit192 <minhanthongminh@gmail.com>
2024-05-18 17:43:18 +0700

    fix

--- backend/api.py
+++ backend/api.py
@@ -5,44 +5,33 @@ import recommender
 
 app = FastAPI()
 
+
 class AudioFeatures(BaseModel):
-    acousticness : float
-    danceability : float
-    duration_ms : float
-    energy : float
-    instrumentalness : float
-    key : int
-    liveness : float
-    loudness : float
-    mode : int
-    speechiness : float
-    tempo : float
-    time_signature : int
-    valence : float
+    acousticness: float
+    danceability: float
+    duration_ms: float
+    energy: float
+    instrumentalness: float
+    key: int
+    liveness: float
+    loudness: float
+    mode: int
+    speechiness: float
+    tempo: float
+    time_signature: int
+    valence: float
+
 
 class TrackID(BaseModel):
-    id : list
+    id: list
+
 
 class TrackInfos(BaseModel):
-    id : str
-    artist : str
-    track_name : str
+    id: str
+    artist: str
+    track_name: str
 
-# def process_json_input(json_str:dict):
-#     unneccesary_feats = ["id", "uri", "track_href", "analysis_url", "type"]
-#     input_ID = json_str["id"]
-
-#     for feat in json_str:
-
-@app.get("/recommend", response_model=TrackID, status_code=status.HTTP_200_OK)
-async def get_recommended_tracks(input_track_feats: AudioFeatures):
-    """
-    Hàm nhận features 1 bài nhạc để nhận lại IDs đề xuất của các bài nhạc khác
-    Status code:
-    - 201: Thành công.
-    - 422: Truyền dữ liệu không hợp lệ.
-    """
-    # print(input_track_feats.dict())
+def process_json_input(input_track_feats):
     dict = input_track_feats.dict()
     drops = ["key", "mode", "time_signature"]
     for key in drops:
@@ -50,15 +39,27 @@ async def get_recommended_tracks(input_track_feats: AudioFeatures):
     print(dict)
     input_vector = [dict[key] for key in dict.keys()]
 
-    # print(input_vector)
+    return input_vector
+
+
+@app.get("/recommend", response_model=TrackID, status_code=status.HTTP_200_OK)
+async def get_recommended_tracks(input_track_feats: AudioFeatures):
+    """
+    Hàm nhận features 1 bài nhạc để nhận lại IDs đề xuất của các bài nhạc khác
+    Status code:
+    - 200: Thành công.
+    - 422: Truyền dữ liệu không hợp lệ.
+    """
+    input_vector = process_json_input(input_track_feats)
     return recommender.get_instance().get_recommendation_ID(input_vector)
+
 
 @app.get("/{id}", response_model=TrackInfos, status_code=status.HTTP_200_OK)
 async def get_tracks_infos_id(input_id: TrackID):
     """
     Hàm nhận id 1 bài nhạc để nhận lại thông tin bài nhạc đó gồm track_name và artist
     Status code:
-    - 201: Thành công.
+    - 200: Thành công.
     - 422: Truyền dữ liệu không hợp lệ.
     """
     return recommender.get_tracks_infos(input_id)
