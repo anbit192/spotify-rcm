680fa0fc2de86a30c0bcf67566bb454b47a6e8af
anbit192 <minhanthongminh@gmail.com>
2024-05-18 17:39:59 +0700

    main api

--- backend/api.py
+++ backend/api.py
@@ -21,14 +21,20 @@ class AudioFeatures(BaseModel):
     valence : float
 
 class TrackID(BaseModel):
-    id : str
+    id : list
 
 class TrackInfos(BaseModel):
     id : str
     artist : str
     track_name : str
 
-@app.get("/recommend", response_model=AudioFeatures, status_code=status.HTTP_200_OK)
+# def process_json_input(json_str:dict):
+#     unneccesary_feats = ["id", "uri", "track_href", "analysis_url", "type"]
+#     input_ID = json_str["id"]
+
+#     for feat in json_str:
+
+@app.get("/recommend", response_model=TrackID, status_code=status.HTTP_200_OK)
 async def get_recommended_tracks(input_track_feats: AudioFeatures):
     """
     Hàm nhận features 1 bài nhạc để nhận lại IDs đề xuất của các bài nhạc khác
@@ -36,7 +42,16 @@ async def get_recommended_tracks(input_track_feats: AudioFeatures):
     - 201: Thành công.
     - 422: Truyền dữ liệu không hợp lệ.
     """
-    return recommender.get_recommendations_IDs(input_track_feats)
+    # print(input_track_feats.dict())
+    dict = input_track_feats.dict()
+    drops = ["key", "mode", "time_signature"]
+    for key in drops:
+        dict.pop(key, None)
+    print(dict)
+    input_vector = [dict[key] for key in dict.keys()]
+
+    # print(input_vector)
+    return recommender.get_instance().get_recommendation_ID(input_vector)
 
 @app.get("/{id}", response_model=TrackInfos, status_code=status.HTTP_200_OK)
 async def get_tracks_infos_id(input_id: TrackID):
