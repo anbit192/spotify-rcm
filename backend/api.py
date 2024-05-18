d58fecdb00f8c08aac2334acc4025646c6677be5
anbit192 <minhanthongminh@gmail.com>
2024-05-18 20:54:28 +0700

    add cors

--- backend/api.py
+++ backend/api.py
@@ -2,8 +2,17 @@ from pydantic import BaseModel
 from fastapi import FastAPI, status
 from typing import List
 import recommender
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 class AudioFeatures(BaseModel):
